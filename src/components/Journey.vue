8e38134ece3337f261ff735b81eb7373d0795dfb
raphe-dev <raph0713@gmail.com>
2019-04-07 15:00:52 -0400

    better parallax

--- src/components/Journey.vue
+++ src/components/Journey.vue
@@ -1,27 +1,32 @@
 <template>
-  <v-container >
-      <h1> My journey </h1>
-    <v-timeline>
-      <v-timeline-item
-        v-for="(year, i) in years"
-        :key="i"
-        :color="year.color"
-        :icon="year.icon"
-      >
-        <template v-slot:opposite>
-          <span
-            :class="`headline font-weight-bold ${year.color}--text`"
-            v-text="year.year"
-          ></span>
-        </template>
-        <div class="py-3">
-          <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">{{year.title}}</h2>
-          <div class="text--secondary"> 
-            Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+  <v-container fluid>
+    <v-layout row wrap align-content-space-between>
+      <v-flex
+        text-sm-left >
+        <h1> My journey </h1>
+      </v-flex>
+      <v-timeline>
+        <v-timeline-item
+          v-for="(year, i) in years"
+          :key="i"
+          :color="year.color"
+          :icon="year.icon"
+        >
+          <template v-slot:opposite>
+            <span
+              :class="`headline font-weight-bold ${year.color}--text`"
+              v-text="year.year"
+            ></span>
+          </template>
+          <div class="py-3">
+            <h2 :class="`headline font-weight-light mb-3 ${year.color}--text`">{{year.title}}</h2>
+            <div class="text--secondary"> 
+              Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+            </div>
           </div>
-        </div>
-      </v-timeline-item>
-    </v-timeline>
+        </v-timeline-item>
+      </v-timeline>
+    </v-layout>
   </v-container>
 </template>
 
